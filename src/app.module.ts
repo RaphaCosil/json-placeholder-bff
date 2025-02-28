6b336c9ea2328cb1840074288fcdb874c48a001e
RaphaCosil <Raphael.Cosil@gmail.com>
2025-02-27 21:59:08 -0300

    Configuring Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { PublicationModule } from './publication/publication.module';
-import { CommentModule } from './comment/post.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
